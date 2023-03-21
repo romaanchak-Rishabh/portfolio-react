271edbd5c5e774024f77a55f2f291adfafc54704
Rishabh Tater <rishabhtater14@gmail.com>
2023-03-21 14:44:49 +0530

    added pics and links to projects

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -2,12 +2,12 @@ import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
 import useMediaQuery from "../hooks/useMediaQuery";
 import ProjectImg1 from "../assets/project-1.jpeg";
-import ProjectImg2 from "../assets/project-2.jpeg";
-import ProjectImg3 from "../assets/project-3.jpeg";
-import ProjectImg4 from "../assets/project-4.jpeg";
-import ProjectImg5 from "../assets/project-5.jpeg";
-import ProjectImg6 from "../assets/project-6.jpeg";
-import ProjectImg7 from "../assets/project-7.jpeg";
+import ProjectImg2 from "../assets/project-10.png";
+import ProjectImg3 from "../assets/project-9.png";
+import ProjectImg4 from "../assets/project-11.png";
+import ProjectImg5 from "../assets/project-12.jpeg";
+import ProjectImg6 from "../assets/project-8.png";
+import ProjectImg7 from "../assets/project-13.jpeg";
 
 
 const container = {
@@ -22,8 +22,8 @@ const projectVariant = {
     visible: { opacity: 1, scale: 1}
 }
 
-const Project = ( {title, Imgsrc} ) => {
-    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`
+const Project = ( {title, Imgsrc, Placeholder} ) => {
+    const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-2 md:p-16 text-deep-blue`
     const projectTitle = title.split(" ").join("-").toLowerCase();
 
     const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
@@ -31,20 +31,20 @@ const Project = ( {title, Imgsrc} ) => {
     return (
         <>
             {isAboveSmallScreens ? (
-                <motion.div variants={projectVariant} className="relative">
+                <motion.div variants={projectVariant} className="relative h-full w-full">
                     <div className={overlayStyles}>
                         <p className="text-2xl font-playfair">{title}</p>
                         <p className="mt-7">
-                            In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
+                            {Placeholder}
                         </p>
                     </div>
-                    <img src={Imgsrc} alt={projectTitle} />
+                    <img src={Imgsrc} className="h-full" alt={projectTitle} />
                 </motion.div>
             ) : (
-                <div variants={projectVariant} className="relative">
+                <div variants={projectVariant} className="relative my-2 md:my0">
                     <div className={overlayStyles}>
-                        <p className="text-2xl font-playfair">{title}</p>
-                        <p className="mt-7">
+                        <p className="text-xl md:text-2xl font-playfair">{title}</p>
+                        <p className="text-base mt-4 md:mt-7">
                             In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
                         </p>
                     </div>
@@ -80,6 +80,8 @@ const Projects = () => {
                 </div>
                 <p className="mt-5 mb-0">
                     Here's some of the ideas that I brought into life!
+                    <br />
+                    Click the projects to go to respective Project pages.
                 </p>
             </motion.div>
 
@@ -96,17 +98,17 @@ const Projects = () => {
                 <div className="flex justify-center text-center items-center p-10 bg-red max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold">
                     BEAUTIFUL USER INTERFACES
                 </div>
-                <Project title="Project 1" Imgsrc={ProjectImg1} />
-                <Project title="Project 2" Imgsrc={ProjectImg2} />
+                <Project title="" Imgsrc={ProjectImg1} Placeholder="" />
+                <a href="https://github.com/romaanchak-Rishabh/Phontek" target="_blank"><Project title="Phontek" Imgsrc={ProjectImg2} Placeholder="I utilized a diverse range of advanced web development technologies, including ReactJS, OpenStreetMap, and Axios, to develop a fully functional and multipage web application for an ecommerce business specializing in technological products." /></a>
 
                 {/* ROW 2 */}
-                <Project title="Project 3" Imgsrc={ProjectImg3} />
-                <Project title="Project 4" Imgsrc={ProjectImg4} />
-                <Project title="Project 5" Imgsrc={ProjectImg5} />
+                <a href="https://codepen.io/romaanchak-Rishabh/pen/jOmPwKv" target="_blank"><Project title="Vegan Store" Imgsrc={ProjectImg3} Placeholder="I developed a frontend website template for a small online food store, effectively showcasing a diverse range of dishes and providing essential information, including the business's address and contact details." /></a>
+                <a href="https://github.com/romaanchak-Rishabh/weather-app" target="_blank"><Project title="Yoganta" Imgsrc={ProjectImg4} Placeholder="Throughout the course of my 3-month internship, I utilized ReactJS to develop a robust web application that boasted a number of key features, including a booking panel, an admin panel, and a real-time price section." /></a>
+                <a href="https://github.com/romaanchak-Rishabh/VenusEdTech" target="_blank"><Project title="Venus Ed" Imgsrc={ProjectImg5} Placeholder="During an 18-hour intra-college Web Development Hackathon, I leveraged ReactJS to develop a multi-page website for an online education platform, which culminated in securing the second position in the competition." /></a>
 
                 {/* ROW 3 */}
-                <Project title="Project 6" Imgsrc={ProjectImg6} />
-                <Project title="Project 7" Imgsrc={ProjectImg7} />
+                <a href="https://github.com/romaanchak-Rishabh/Apple.com_Clone" target="_blank"><Project title="Apple.com Clone" Imgsrc={ProjectImg6} Placeholder="I successfully replicated the frontend of 'Apple.com', complete with full functionality and responsive design." /></a>
+                <a href="https://github.com/romaanchak-Rishabh/flip-the-script" target="_blank"><Project title="Flip-the-Script" Imgsrc={ProjectImg7} Placeholder="Leveraging a suite of cutting-edge technologies, including ReactJS, Quill, and Axios, I developed a full-stack social media website catering to the needs of both authors and readers. Boasting a range of features, including account management, a user feed, and administrative functionalities." /></a>
                 <div className="flex justify-center text-center items-center p-10 bg-blue max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold">
                     SMOOTH USER EXPERIENCE
                 </div>
